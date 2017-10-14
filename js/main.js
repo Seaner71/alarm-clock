5771bcdbfa3cc8d65d22ccc60df9a83ceaaf307d
echo 'hello world <ssmyth1071@gmail.com>
2017-10-13 23:36:13 -0400

    add random gradient (linear or radial) background

--- js/main.js
+++ js/main.js
@@ -6,32 +6,33 @@ var hrs=date.getHours(); hrs < 10? hrs ='0'+hrs: hrs = date.getHours();
 var mins=date.getMinutes(); mins < 10? mins ='0'+mins: mins = date.getMinutes();
 var secs= date.getSeconds(); secs < 10? secs ='0'+secs: secs = date.getSeconds();
 
-// Random Hex generator
-var hexTime =  '#' +hrs+mins+secs;
+  var hexTime =  '#' +hrs+mins+secs;
   // var normalTime = `${hrs}:${mins}:${secs}`
-  time.innerHTML = hexTime;
+  time.innerHTML = hexTime ;
 }
-
+// var color = document.querySelector("#back-ground");
+// color.style.background = `linear-gradient(#000 ,#a8d, #F8B04F)`;
 
 setInterval(currentTime, 1000);
 
 function randHex() {
     var color = document.querySelector("#back-ground");
+    // empty array to contain 3hex colors and hex
+    var hexArray =[]
     const hexChars = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+    for (let i =0; i <3; i++) {
     let result = '#';
-    for (let i =0; i < 6; i++) {
+    // Random Hex generator
+      for (let i =0; i < 6; i++) {
       var randIndex = Math.floor(Math.random()* hexChars.length);
       result += hexChars[randIndex];
     }
-    color.style.backgroundColor =result;
+    // push each hex created into array
+    hexArray.push(result)
+    }
+    // set backround to linear-gradient or radial-gradient randomly using values from hexArray
+    gradientArray =['linear-gradient','radial-gradient'];
+    randGradient = gradientArray[Math.floor(Math.random()*2)];
+    color.style.background = `${randGradient}(${hexArray[0]},${hexArray[1]},${hexArray[2]})`;
 }
 setInterval(randHex, 10000)
-// change background image based on time of day
-/*
-if (mins === '09') {
-color.style.classList ='.dawn'
-time.innerHTML = normalTime + '<br>' + "Good Afternoon!";
-} else {
-color.style.classList = '';
-}
-*/
